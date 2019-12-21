d855701284e19399e3a7d84d42166936ba4049e8
h0mieSapiens <51687450+h0mieSapiens@users.noreply.github.com>
2019-12-20 18:42:27 -0800

    Add files via upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,17 +4,24 @@ const Task = require("../models/task");
 
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const tasks = await Task.find();
 
-    res.render('index.ejs');
+    res.render('index.ejs', {
+        tasks
+
+    });
 });
 
 router.post('/add', async (req, res) => {
-   
-    const task=new Task(req.body);
+
+    const task = new Task(req.body);
+
+
     await task.save();
 
-    res.send("recivido");
+
+    res.redirect('/');
 
 
 });
@@ -23,6 +30,47 @@ router.get('/Todo', (req, res) => {
 
     res.render('todo.html');
 });
+router.get('/turn/:id', async (req, res, next) => {
+    let {
+        id
+    } = req.params;
+    const task = await Task.findById(id);
+    task.status = !task.status;
+    await task.save();
+    res.redirect('/');
+});
+
+
+router.get('/edit/:id', async (req, res, next) => {
+    const task = await Task.findById(req.params.id);
+    console.log(task)
+    res.render('edit', {
+        task
+    });
+});
+
+router.post('/edit/:id', async (req, res, next) => {
+    const {
+        id
+    } = req.params;
+    await Task.update({
+        _id: id
+    }, req.body);
+    res.redirect('/');
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const {
+        id
+    } = req.params;
+    await Task.remove({
+        id: id
+    });
+    res.redirect('/');
+
+
+});
+
 
 module.exports = router;
 console.log("routes registered");
